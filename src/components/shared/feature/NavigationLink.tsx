818c1173a7c0bdb98decd82cb8b966485c33a674
kimiafarshadnia <farshadniakimia@gmail.com>
2025-03-12 18:18:27 +0330

    fixed bug in NavigationLink

--- src/components/shared/feature/NavigationLink.tsx
+++ src/components/shared/feature/NavigationLink.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { LinkProps } from 'next/link';
-import { Link } from '@/navigation';
+import Link, { LinkProps } from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { UrlObject } from 'url';
 
 interface CustomLinkProps extends LinkProps {
   children: ReactNode;
-  href: string | UrlObject;
+  href: string | (UrlObject & { pathname: string });
 }
 
 export default function NavigationLink({ href, children, ...rest }: CustomLinkProps) {
   const selectedLayoutSegment = useSelectedLayoutSegment();
   const currentPathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : '/';
 
-  const getHrefPathname = (href: string | UrlObject): string => {
-    if (typeof href === 'string') {
-      return href;
-    } else {
-      return href.pathname ?? '/';
-    }
+  const getHrefPathname = (href: string | (UrlObject & { pathname: string })): string => {
+    return typeof href === 'string' ? href : href.pathname;
   };
 
   const isActive = currentPathname === getHrefPathname(href);
@@ -29,10 +24,11 @@ export default function NavigationLink({ href, children, ...rest }: CustomLinkPr
     <Link
       aria-current={isActive ? 'page' : undefined}
       href={href}
-      className={`duration-200 ${isActive
+      className={`duration-200 ${
+        isActive
           ? 'text-black dark:text-white'
           : 'text-gray-500 dark:text-gray-300 hover:text-black hover:dark:text-white'
-        }`}
+      }`}
       {...rest}
     >
       {children}
